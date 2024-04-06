2148b3b577fc00051348db59adf86b64f888085f
unknown <baraahedri69@gmail.com>
2024-04-06 08:46:58 +0100

    dojo survey

--- flask/fundamentals/Core/Dojo Survey/server.py
+++ flask/fundamentals/Core/Dojo Survey/server.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'Ouch'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
+    return render_template("index.html")
+
+@app.route('/process',methods=['POST'])
+def process(): 
         session['name'] = request.form['name']
         session['location'] = request.form['location']
         session['language'] = request.form['language']
         session['comment'] = request.form['comment']
-        return redirect(url_for('result'))
-    return render_template('index.html')
+        return redirect('/result')
 
 @app.route('/result')
 def result():
-    return render_template('result.html', name=session.get('name'), location=session.get('location'),
-                           language=session.get('language'), comment=session.get('comment'))
+    name = session.get('name')
+    location = session.get('location')
+    language = session.get('language')
+    comment = session.get('comment')
+    return render_template('result.html',name=name, location=location, language=language, comment=comment)
 
 if __name__ == '__main__':
     app.run(debug=True)
